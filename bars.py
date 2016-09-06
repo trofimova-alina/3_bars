96523906df27d4cd2351826d66bab877e32a5d54
Alina <trofimova.alinka@yandex.ru>
2016-09-06 19:14:38 +0000

    Moscow bars

--- bars.py
+++ bars.py
@@ -2,20 +2,47 @@ import json
 
 
 def load_data(filepath):
-    pass
+    return json.load( open(filepath, 'r') )
 
 
 def get_biggest_bar(data):
-    pass
+    max_seatscount = 0
+    huge_bar = None
+    for bar in data:
+        seatscount = bar['Cells']['SeatsCount']
+        if seatscount > max_seatscount: 
+            max_seatscount = seatscount  
+            huge_bar = bar
+    return huge_bar
 
 
 def get_smallest_bar(data):
-    pass
+    min_seatscount = data[0]['Cells']['SeatsCount']
+    small_bar = None
+    for bar in data:
+        seatscount = bar['Cells']['SeatsCount']
+        if seatscount < min_seatscount: 
+            min_seatscount = seatscount  
+            small_bar = bar
+    return small_bar
 
 
 def get_closest_bar(data, longitude, latitude):
-    pass
+    closest_bar = data[0]
+    long, lat = data[0]['Cells']['geoData']['coordinates']  
+    min_distance = ((long - longitude) ** 2 + (lat - latitude) ** 2) ** 0.5
+    for bar in data:
+        long, lat = bar['Cells']['geoData']['coordinates']  
+        distance_to_bar = ((long - longitude) ** 2 + (lat - latitude) ** 2) ** 0.5
+        if distance_to_bar < min_distance:
+            closest_bar = bar
+    return closest_bar        
 
 
 if __name__ == '__main__':
-    pass
+    data = load_data('bars.json')
+    print (get_biggest_bar(data))
+    print (get_smallest_bar(data))
+    longitude = float(input('Enter longitude: '))
+    latitude = float(input('Enter latitude: '))
+    print (get_closest_bar(data, longitude, latitude))
